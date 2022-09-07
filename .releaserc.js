f7ccc871e810360e9b121e67c45f13814fda6382
arriere-aride <arriere.aride@gmail.com>
2022-09-07 19:26:47 +0200

    chore(release): add semantic release
    Semantic release will generate release and changelog based on commit

---
+++ .releaserc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    plugins: [
+      [
+        '@semantic-release/commit-analyzer',
+        {
+          // https://github.com/semantic-release/commit-analyzer#releaserules
+          releaseRules: [
+            { scope: 'no-release', release: false },
+            { breaking: true, release: 'major' },
+            { type: 'feat', release: 'minor' },
+            { type: 'refactor', scope: 'core-*', release: 'minor' },
+            { type: '*', release: 'patch' },
+          ],
+        },
+      ],
+      '@semantic-release/release-notes-generator',
+      '@semantic-release/github',
+    ],
+  };
