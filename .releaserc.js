a386964be6fb952b670634dbf8e39677de69a362
arriere-aride <arriere.aride@gmail.com>
2022-09-08 08:53:21 +0200

    ci(circleci): add github semantic release

--- .releaserc.js
+++ .releaserc.js
@@ -15,6 +15,7 @@ module.exports = {
         ],
       },
     ],
-    "@semantic-release/release-notes-generator"
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/github",
   ],
 };
