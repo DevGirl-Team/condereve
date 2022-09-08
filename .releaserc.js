3a49e5f7fd3eb516d99cc620d992cff216ef0c80
arriere-aride <112582032+arriere-aride@users.noreply.github.com>
2022-09-08 12:04:29 +0200

    Circle ci (#3)
    * ci(circleci): add semantic release
    
    * ci(circleci): updates semantic release
    
    * ci(circleci): set-up semantic release in ci
    
    * ci(circleci): updates repository prop to match ci needs
    
    * ci(circle-ci): updates repository url in package.json
    
    Used by semantic-release/github to find the repo
    
    * ci(circle-ci): setup semantic-release on circle-ci via cli
    
    * ci(circle-ci): update .releaserc.js
    
    Add repositoryUrl and branches
    
    * ci(circle-ci): removes github plugin
    
    * ci(circle-ci): add actions/checkout
    
    * ci(circle-ci): uses release config
    
    see https://semantic-release.gitbook.io/semantic-release/v/beta/recipes/recipes/circleci-workflows
    
    * ci(circle-ci): use yarn instead of npm
    
    * ci(circle-ci): use yarn with npx
    
    * ci(circle-ci): updates yarn to npm
    
    To complicated to use circle-ci with yarn, back to npm
    
    * ci(circle-ci): add package-lock.json
    
    * ci(circle-ci): set yarn as package manager
    
    Now that a package-lock.json has been provided, we don’t need to use npm anymore
    
    * ci(circle-ci): add lock files
    
    * ci(circleci): add github semantic release
    
    * ci(circle-ci): add changelog
    
    * ci(circle-ci): add config to circle-ci
    
    * ci(circle-ci): add lock files in front

--- .releaserc.js
+++ .releaserc.js
@@ -1,19 +1,22 @@
 module.exports = {
-    plugins: [
-      [
-        '@semantic-release/commit-analyzer',
-        {
-          // https://github.com/semantic-release/commit-analyzer#releaserules
-          releaseRules: [
-            { scope: 'no-release', release: false },
-            { breaking: true, release: 'major' },
-            { type: 'feat', release: 'minor' },
-            { type: 'refactor', scope: 'core-*', release: 'minor' },
-            { type: '*', release: 'patch' },
-          ],
-        },
-      ],
-      '@semantic-release/release-notes-generator',
-      '@semantic-release/github',
+  repositoryUrl: "https://github.com/DevGirl-Team/condereve.git",
+  branches: ["main", "master", "next", "circle-ci"],
+  plugins: [
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        // https://github.com/semantic-release/commit-analyzer#releaserules
+        releaseRules: [
+          { scope: "no-release", release: false },
+          { breaking: true, release: "major" },
+          { type: "feat", release: "minor" },
+          { type: "refactor", scope: "core-*", release: "minor" },
+          { type: "*", release: "patch" },
+        ],
+      },
     ],
-  };
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+    "@semantic-release/github",
+  ],
+};
